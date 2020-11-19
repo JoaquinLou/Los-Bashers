6f4d9b0fc83dafd4fc493211fb61d0ee4a9f511b
Joaquin Lou <joaquinlouh@gmail.com>
2020-11-19 20:22:37 +0100

    Segunda prueba hacking

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,15 +1,21 @@
-#!bin/bash
+#!/bin/bash
 echo "¿A qué equipo quieres hackear?"
 read team_name
+
+rm -r ref_repo
 
 mkdir ref_repo
 
 declare -A repos
 
-repos[CAMPECHANO]='https://github.com/rgomezh/campechano.git'
-repos[GOODSHELLERS]='https:github.com/ruizber23/goodshellers.git'
-repos[FANTÁSTICOS]='https://github.com/hxn241/Fantasticos.git'
-repos[EQUIPO_CONFINADOS]='https://github.com/danielgarciasanchez/Equipo_confinados'
-repos[TEAM5]='https://github.com/rbarlaam/Team5'
+repos["campechano"]='https://github.com/rgomezh/campechano.git'
+repos["goodshellers"]='https:github.com/ruizber23/goodshellers.git'
+repos["fantasticos"]='https://github.com/hxn241/Fantasticos.git'
+repos["equipo_confinados"]='https://github.com/danielgarciasanchez/Equipo_confinados'
+repos["team5"]='https://github.com/rbarlaam/Team5'
 
-echo ${repos[team_name]}
+echo "Hackeando..... su repositorio es " ${repos[$team_name]}
+
+git clone ${repos[$team_name]} ./ref_repo
+
+
