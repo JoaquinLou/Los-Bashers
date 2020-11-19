9fe2ff540110e98b58f80695e5ad69bb6a353b17
Victor Carrillo <victorcarrillonavarro@gmail.com>
2020-11-19 19:31:12 +0100

    Ejercicio 6 out

--- 006.sh
+++ 006.sh
@@ -5,4 +5,5 @@ read age
 a=5
 age=$(($age + $a))
 echo "Tu edad en 5 años será $age y aún no tendremos la vacuna del Covid24"
+echo "Tu edad en 5 años será $age y aún no tendremos la vacuna del Covid24" > 006.out
 
